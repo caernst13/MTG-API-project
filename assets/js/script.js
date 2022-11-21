4e95697475d533551d00539c943243384ec4bf6a
caernst13 <caleb_ernst@yahoo.com>
2022-11-21 17:49:16 -0500

    added functionality to the other search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
 var nameFormEl = document.querySelector('#nameSearch')
 var cardNameEl = document.querySelector('#cardName')
-function testing() {
-    var requestURL = 'http://api.magicthegathering.io/v1/cards'
+var otherSearchEl = document.querySelector('#otherSearch')
+var cardTypeEl = document.querySelector('#cardType')
+var cmcEl = document.querySelector('#cmc')
+// function testing() {
+//     var requestURL = 'http://api.magicthegathering.io/v1/cards'
 
-    fetch(requestURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-    });
-};
+//     fetch(requestURL)
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data)
+//     });
+// };
 
-testing();
+// testing();
 
-    
 var nameSearch = function (event) {
     event.preventDefault();
     var cardName = cardNameEl.value.trim();
@@ -22,4 +24,23 @@ var nameSearch = function (event) {
     console.log('test')
 };
 
+var otherSearch = function (event) {
+    event.preventDefault();
+
+    var checkedEl = $('input:checked');
+    var selected = [];
+
+    $.each(checkedEl, function () {
+        selected.push($(this).val());
+    });
+    if (selected.length > 0) {console.log(selected)};
+
+    var cardType = cardTypeEl.value;
+    console.log(cardType);
+
+    var cmc = cmcEl;
+    if (Number.isInteger(cmc)) {console.log(cmc)};
+}
+
 nameFormEl.addEventListener('submit', nameSearch);
+otherSearchEl.addEventListener('submit', otherSearch);
